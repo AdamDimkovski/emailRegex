e7ed5fd8d667fe2ac013fbee4d47d6dba5c9b1e1
StudentMan558 <s4168373@student.rmit.edu.au>
2026-08-24 00:23:25 +1000

    Added malicious domain detection with dnspython

--- regex.py
+++ regex.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import dns.resolver
+
 import re
 
 app = Flask(__name__)
@@ -10,19 +12,42 @@ def input():
 @app.route("/regex.py", methods=["GET", "POST"])
 def regex():
     message = None
+    result_gif = None
+
     if request.method == "POST":
         email = request.form.get("email")
         if email:
            email_pattern = r"^[a-zA-Z0-9]+([._%+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+([-][a-zA-Z0-9]+)*([.][a-zA-Z]{2,})+$"
            if re.fullmatch(email_pattern, email):
-                   message = f"Valid Email Alert! {email}"
-                   result_gif = "success.gif"
+
+                    # IF email format is valid, check to see if its on a blocklist
+                   domain_to_check = email.partition('@')[2]
+                   dbl_zone = "dbl.spamhaus.org"
+                   query_target = f"{domain_to_check}.{dbl_zone}"
+                   
+                   try:
+                        dns.resolver.resolve(query_target, "A")
+                        
+                        # This runs if the email is malicious 
+                        message = None
+                        result_gif = "hacker.gif"
+                        
+                   # This runs if the email is not on the dbl block list
+                   except dns.resolver.NXDOMAIN:
+                        message = f"Valid Email Alert! {email}"
+                        result_gif = "success.gif"
+                        
+                   # This runs if there is an error checking domains
+                   except Exception as e:
+                        message = "Internal Error Whoopidy Doo Dah"
+                        result_gif = "error.gif"
            else:
                message = "wrong format bud"
                result_gif = "failed.png" 
         else: 
             message = "Internal Error Whoopidy Doo Dah"
             result_gif = "error.gif"
+
         
     return render_template("index.html", message=message, result_gif=result_gif)
 
