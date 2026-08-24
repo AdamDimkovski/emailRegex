02d5a8b82c44b2a641b9b1dde7eb90471d765f25
StudentMan558 <s4168373@student.rmit.edu.au>
2026-08-24 13:54:16 +1000

    Updated Comments and Added Sound effects for all cases

--- regex.py
+++ regex.py
@@ -1,3 +1,5 @@
+# Regex.py: a flask file which connects backend to frontend, created by Adam Dimkovski [24/08/2026]
+# Imports used in backend
 from flask import Flask, render_template, request
 import dns.resolver
 
@@ -5,22 +7,37 @@ import re
 
 app = Flask(__name__)
 
+# App route connecting index.html to regex.py
 @app.route('/')
 def input():
     return render_template('index.html')
 
+# App route allowing for Get and Post Request
 @app.route("/regex.py", methods=["GET", "POST"])
+
+# Method which runs when textfield is submitted
 def regex():
+    
+    # Declares both variables as nothing to prevent overlap
     message = None
     result_gif = None
 
+    # Checks for Post request
     if request.method == "POST":
+        
+        # Collects email from textfield
         email = request.form.get("email")
+        
+        # When Email is collected
         if email:
+            
+           # Email format regex
            email_pattern = r"^[a-zA-Z0-9]+([._%+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+([-][a-zA-Z0-9]+)*([.][a-zA-Z]{2,})+$"
+           
+           # Match email if format is followed
            if re.fullmatch(email_pattern, email):
 
-                    # IF email format is valid, check to see if its on a blocklist
+                   # If email format is valid, check to see if its on a blocklist
                    domain_to_check = email.partition('@')[2]
                    dbl_zone = "dbl.spamhaus.org"
                    query_target = f"{domain_to_check}.{dbl_zone}"
@@ -41,14 +58,18 @@ def regex():
                    except Exception as e:
                         message = "Internal Error Whoopidy Doo Dah"
                         result_gif = "error.gif"
+                        
+           # This runs if the email format is incorrect             
            else:
                message = "wrong format bud"
                result_gif = "failed.png" 
+               
+        # This runs if there is an internal error      
         else: 
             message = "Internal Error Whoopidy Doo Dah"
             result_gif = "error.gif"
 
-        
+    # Returns index.html after reload with correctly passed html request    
     return render_template("index.html", message=message, result_gif=result_gif)
 
 if __name__ == "__main__":
